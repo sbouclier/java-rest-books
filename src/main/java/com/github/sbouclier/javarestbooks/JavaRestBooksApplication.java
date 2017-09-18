9edf606a242dd090f58e9ec275c9d10b0b883b76
Stéphane Bouclier <sbouclier@users.noreply.github.com>
2017-09-18 21:20:05 +0200

    spring init

---
+++ src/main/java/com/github/sbouclier/javarestbooks/JavaRestBooksApplication.java
@@ -0,0 +1,12 @@
+package com.github.sbouclier.javarestbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestBooksApplication.class, args);
+	}
+}
